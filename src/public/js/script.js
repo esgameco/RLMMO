ce39d2715c81ab5c29fd487bc94d2f7a302c6506
Chance Vodnoy <esgameco@gmail.com>
2021-06-27 12:36:23 -0700

    Added api access

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
-    
+    const catForm = document.getElementById('cats');
+    const catSubmit = catForm.elements['submit'];
+    const catOutput = catForm.elements['output']
+
+    catSubmit.addEventListener('click', async () => {
+        const res = await fetch('/api/cats');
+        const resJson = await res.json();
+        catOutput.innerHTML = resJson.cats;
+    });
 });
